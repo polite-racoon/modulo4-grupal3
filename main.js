584038ff0a7a4bad6a531026987e539f1e3d44c8
galiardi <66950896+galiardi@users.noreply.github.com>
2023-05-25 11:58:46 -0400

    Agrega listener Registro Importacion y Tabla donde mostrar registros

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
+// Formulario Registro de Empresa
 const formularioEmpresa = document.getElementById('formularioEmpresa');
-// Inputs Formulario Registro de Empresa
-//  nombre empresa (string) - rut (string) - rubro (string) - tamaño (string)
 const nombreEmpresa = document.getElementById('nombreEmpresa');
 const rut = document.getElementById('rut');
 const rubro = document.getElementById('rubro');
 const tamano = document.getElementById('tamano');
 
+// Formulario Registro de importaciones
 const formularioImportacion = document.getElementById('formularioImportacion');
-// Inputs Formulario Registro de importaciones
-// div vacio
 const empresasSelectElement = document.getElementById('empresasSelectElement');
 const productosDiv = document.getElementById('productosDiv');
 
+// Formulario Registro de productos
 const formularioProducto = document.getElementById('formularioProducto');
-// Inputs Formulario Registro de productos
-// nombre (string) - cantidad (numero) - precio (numero)
 const nombreProducto = document.getElementById('nombreProducto');
 const cantidadProducto = document.getElementById('cantidadProducto');
 const precioProducto = document.getElementById('precioProducto');
+
+// Mostrar Empresas Registradas
+const empresasRegistradas = document.getElementById('empresasRegistradas');
 
 // data
 const listaDeProductos = [];
@@ -34,22 +34,85 @@ formularioEmpresa.addEventListener('submit', (e) => {
   );
   listaDeEmpresas.push(nuevaEmpresa);
 
+  // agrega un option en el seleccionador de empresas
   const optionElement = document.createElement('option');
-  optionElement.innerHTML = `<option value="${nuevaEmpresa}">${nuevaEmpresa.nombre}</option>`;
+  optionElement.value = nuevaEmpresa.idRegistro;
+  optionElement.textContent = nuevaEmpresa.nombre;
 
   empresasSelectElement.appendChild(optionElement);
+
+  // agrega una empresa en la tabla de empresas registradas
+
+  const fila = document.createElement('div');
+  fila.className = 'row';
+  fila.innerHTML = `
+  <div class="col">${nuevaEmpresa.idRegistro}</div>
+  <div class="col">${nuevaEmpresa.nombre}</div>
+  <div class="col">${nuevaEmpresa.rut}</div>
+  <div class="col">${nuevaEmpresa.rubro}</div>
+  <div class="col">${nuevaEmpresa.tamano}</div>
+  <div id="${nuevaEmpresa.idRegistro}" class="col"></div>`;
+
+  empresasRegistradas.appendChild(fila);
+
+  // limpia formulario
   nombreEmpresa.value = '';
   rut.value = '';
   rubro.value = '';
   tamano.value = '';
-
 });
 
 formularioImportacion.addEventListener('submit', (e) => {
   e.preventDefault();
-  const importacion = new Importacion();
-  // debes pasarle a Importacion los productos que podras elegir desde el formulario
-  // agrega algunos productos desde la pagina para que veas como se ven
+
+  if (!empresasSelectElement.value) {
+    alert('Debe seleccionar una empresa');
+    return;
+  }
+
+  checkboxes = document.querySelectorAll('.productoCheckbox'); // NodeList object
+  // console.log(checkboxSelecionados);
+  // console.log(Array.from(checkboxSelecionados));
+  // console.log(Object.values(checkboxSelecionados));
+  const idsProductosSelecionados = [];
+  checkboxes.forEach((checkbox) => {
+    if (checkbox.checked) idsProductosSelecionados.push(checkbox.value);
+  });
+
+  // creamos un objeto con los productos indexados por id,
+  // de esta forma evitamos usar array.filter() junto con array.includes(), lo cual podria resultar ineficiente
+  // para esta aplicacion es exagerado, pero lo hicimos por aprender
+
+  const productosPorId = listaDeProductos.reduce(
+    (acc, el) => ({ ...acc, [el.idProducto]: el }),
+    {}
+  );
+
+  const productosSelecionados = idsProductosSelecionados.map(
+    (id) => productosPorId[id]
+  );
+
+  // Objeto importacion
+  const importacion = new Importacion(productosSelecionados);
+
+  // Objeto de la empresa seleccionada
+  const empresa = listaDeEmpresas.find(
+    (empresa) => empresa.idRegistro === Number(empresasSelectElement.value)
+  );
+
+  empresa.agregarImportacion(importacion);
+
+  const cuadro = document.getElementById(empresa.idRegistro);
+  const div = document.createElement('div');
+  div.addEventListener('click', function () {
+    alert(`valor: ${importacion.obtenerValor()}`);
+  });
+  div.innerHTML = importacion.idImportacion;
+  cuadro.appendChild(div);
+
+  // limpia formulario
+  empresasSelectElement.value = '';
+  checkboxes.forEach((checkbox) => (checkbox.checked = false));
 });
 
 formularioProducto.addEventListener('submit', (e) => {
@@ -66,9 +129,7 @@ formularioProducto.addEventListener('submit', (e) => {
   const checkboxDiv = document.createElement('div');
   checkboxDiv.style = 'font-size: 0.6rem;';
   checkboxDiv.innerHTML = `
-    <input id="${idProducto}" type="checkbox" value="${JSON.stringify(
-    producto
-  )}">
+    <input class="productoCheckbox" id="${idProducto}" type="checkbox" value='${idProducto}'>
     <label for="${idProducto}">${nombre}/ precio: $${precio}/ cantidad: ${cantidad}/ total: ${producto.obtenerValor()}</label>
     <br></br>
     `;
@@ -201,6 +262,7 @@ class Importacion {
   }
 
   obtenerValor() {
+    console.log('calculando valor');
     return this.#productos.reduce(
       (acc, producto) => acc + producto.obtenerValor(),
       0
@@ -242,67 +304,7 @@ class Producto {
     return this.#cantidad * this.#precio;
   }
 }
-// Start
-// if (listaDeEmpresas.length === 0) {
-//   empresasSelectElement.innerHTML = <option value="">--No hay empresas registradas--</option>
-// } else {
 
-// }
-
-// const aduana = new Aduana();
-// const empresa1 = new Empresa(
-//   12345678,
-//   'AgroHolman Limitada',
-//   '75.123.123-0',
-//   'agrícola',
-//   'pequeño'
-// );
-// const empresa2 = new Empresa(
-//   12341234,
-//   'Calerita Dynamics Inc',
-//   '76.000.000-0',
-//   'tecnología',
-//   'mediano'
-// );
-
-// aduana.registrarEmpresa(empresa1);
-// aduana.registrarEmpresa(empresa2);
-
-// const producto1 = new Producto('Maceta VCG 10', 50000, 40);
-// const producto2 = new Producto('Maceta VCG 15', 20000, 70);
-// const producto3 = new Producto('Arduino Uno', 1000, 10000);
-
-// const importacion1 = new Importacion([producto1, producto2]);
-
-// const importacion2 = new Importacion();
-// importacion2.agregarProducto(producto3);
-
-// empresa1.agregarImportacion(importacion1);
-// empresa2.agregarImportacion(importacion2);
-
-// aduana.empresas.forEach((empresa) => {
-//   console.log(
-//     `La empresa ${empresa.nombre}, rut ${empresa.rut}, id de registro ${empresa.idRegistro
-//     }, ha realizado ${empresa.obtenerCantidadDeImportaciones()} importaciones, por un valor total de $${empresa.obtenerValorTotalDeImportaciones()}.`
-//   );
-// });
-
-// console.log(
-//   'Empresas clasificadas por id de registro:\n',
-//   aduana.clasificarEmpresas('idRegistro')
-// );
-
-// console.log(
-//   'Empresas clasificadas por nombre:\n',
-//   aduana.clasificarEmpresas('nombre')
-// );
-
-// console.log(
-//   'Empresas clasificadas por rut:\n',
-//   aduana.clasificarEmpresas('rut')
-// );
-
-// console.log(
-//   'Registro de importaciones por empresa: ',
-//   aduana.obtenerImportacionesPorEmpresa()
-// );
+alert(
+  'Registre empresas, luego regisre productos y finalmente registre importación.'
+);
